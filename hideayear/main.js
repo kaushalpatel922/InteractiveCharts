c54ac1967752e85458d40f73eb4f9aaf3e7c0d84
Kaushal Patel <kaushalpatel922@gmail.com>
2017-05-30 11:28:45 -0400

    updates

--- hideayear/main.js
+++ hideayear/main.js
@@ -1,1 +1,40 @@
 console.log('hide a year');
+
+$(function () {
+var data = [
+  ['x', '2012', '2013', '2014', '2015', '2016', '2017'],
+  ['Year vs Amount', 350, 390, 220, 450, 510, 700]
+];
+
+var chart = c3.generate({
+  "bindto":"#chart",
+  "data": {
+    tye: "line",
+    x: 'x',
+    columns: data
+  },
+  "axis": {
+    "x": {
+      "type":"year"
+    }
+  }
+});
+
+//to hide the bad year
+function hide(year) {
+  var index = data[0].indexOf(year);
+  var newData;
+
+  if(index > -1) {
+    newData = data.concat();
+    newData.forEach(function(v) {
+      v.splice(index,1)
+    });
+
+    chart.load({
+      columns: newData,
+      unload: true
+    })
+  }
+}
+});
